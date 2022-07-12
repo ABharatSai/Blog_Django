c42a1695c62db74134dd84ed975706be9a167c7f
Bharat <sai.2@iitj.ac.in>
2022-07-12 12:04:21 +0530

    For heroku purpose

--- Website/settings.py
+++ Website/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abharatsai-blog-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
